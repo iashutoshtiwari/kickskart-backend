fcd7c30283001abd4af8f38611e6550ee846ba82
Ashutosh Tiwari <mail@ashutoshtiwari.co.in>
2025-05-28 13:56:42 +0530

    Initial commit: migrated old backend code from zip

---
+++ src/app.js
@@ -0,0 +1,30 @@
+//Dependency import
+const express = require("express");
+require("./db/mongoose");
+const cors = require("cors");
+
+const userRouter = require("./routers/user");
+const itemRouter = require("./routers/item");
+const cartRouter = require("./routers/cart");
+const orderRouter = require("./routers/order");
+
+//Invoke express
+const app = express();
+app.use(express.json());
+
+//Enable cors
+app.use(cors());
+
+//Specify port
+const port = process.env.PORT;
+
+//Load routes
+app.use(userRouter);
+app.use(itemRouter);
+app.use(cartRouter);
+app.use(orderRouter);
+
+//Listen for connections
+app.listen(port, () => {
+  console.log("Server listening on Port: " + port);
+});
